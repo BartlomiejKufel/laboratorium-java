9dc8a540ec077a21183eb19bacbd7d605759acdf
Bartłomiej Kufel <joloczok123@gmail.com>
2025-03-06 16:48:31 +0100

    lab 3 skończony

--- lab3/src/Tasks/Run.java
+++ lab3/src/Tasks/Run.java
@@ -1,6 +1,9 @@
 package Tasks;
 
 import InputData.InputData;
+import InputData.RandomValue;
+
+import java.util.Arrays;
 
 public class Run {
     TasksLab03 task03 = new TasksLab03();
@@ -16,7 +19,7 @@ public class Run {
         System.out.println("3. Zadanie 3");
         System.out.println("4. Zadanie 4");
         System.out.println("5. Zadanie 5");
-        System.out.println("7. Wyjście");
+        System.out.println("6. Wyjście");
         System.out.println("Wybierz zadanie: ");
         int choose = input.InputInt();
         switch (choose){
@@ -25,30 +28,66 @@ public class Run {
             case 3: Task03Run(); break;
             case 4: Task04Run(); break;
             case 5: Task05Run(); break;
-            case 7: CloseProgram(); break;
+            case 6: CloseProgram(); break;
             default:
                 System.out.println("Błędne dane. Wybierz ponownie."); break;
         }
     }
 
     private void Task01Run() {
-        
+        System.out.println("Podaj liczbę studentów");
+        int n = input.InputInt();
+        int [] points = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Liczba punktów "+(i+1)+" studenta: ");
+            points[i] = input.InputInt();
+        }
+
+        System.out.println("\nŚrednia liczba punktów w grupie: "+ task03.Task01(points, n));
+
     }
 
     private void Task02Run() {
-        
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Podaj liczbę: ");
+            array[i] = input.InputInt();
+        }
+
+        System.out.println("Ilość liczb dodatnich: "+ task03.Task02v1(array));
+        System.out.println("Ilość liczb ujemnych: "+ task03.Task02v2(array));
+
     }
 
     private void Task03Run() {
-        
+        System.out.print("Podaj n:");
+        int n = input.InputInt();
+
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = i+1;
+        }
+        System.out.println("\nCiąg:\n"+ Arrays.toString(array));
+        System.out.println("Suma liczb parzystych w ciągu: "+task03.Task03(array));
     }
 
     private void Task04Run() {
-        
+        int[] array = RandomValue.RandValue(-10, 45, 5);
+
+        System.out.println("\nCiąg:\n"+ Arrays.toString(array));
+        System.out.println("Suma liczb parzystych w ciągu: "+task03.Task03(array));
     }
 
     private void Task05Run() {
-        
+        System.out.println("Podaj słowo do sprawdzenia");
+        String word = input.InputString();
+
+        if(task03.Task05(word)){
+            System.out.println("Słowo "+word+" jest palindromem");
+        }else{
+            System.out.println("Słowo "+word+" nie jest palindromem");
+        }
     }
 
     private void CloseProgram() {
--- lab3/src/Tasks/TasksLab03.java
+++ lab3/src/Tasks/TasksLab03.java
@@ -1,4 +1,52 @@
 package Tasks;
 
 public class TasksLab03 {
+
+    //Zadanie 1
+    public double Task01(int[] points, int n) {
+        double sum =0;
+        int i = 0;
+        while (i < n) {
+            sum += points[i];
+            i++;
+        }
+        return (sum/n);
+    }
+    //Zadanie 2, liczby dodatnie
+    public int Task02v1(int[] array) {
+        int count =0;
+        for (int i: array){
+            if(i>0)
+                count++;
+        }
+        return count;
+    }
+    //Zadanie 2, liczby ujemne
+    public int Task02v2(int[] array) {
+        int count =0;
+        for (int i: array){
+            if(i<0)
+                count++;
+        }
+        return count;
+    }
+    //Zadanie 3 i 4
+    public int Task03(int[] array) {
+        int sum =0;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i]%2==0)
+                sum+=array[i];
+        }
+        return sum;
+    }
+
+    //Zadanie 5
+    public boolean Task05(String word) {
+        for (int i = 0; i < word.length()/2; i++) {
+            if(word.charAt(i)!=word.charAt(word.length()-1-i))
+                return false;
+        }
+
+        return true;
+    }
 }
